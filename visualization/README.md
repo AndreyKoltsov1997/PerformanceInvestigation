e0c18d4f19f7d2a782d0485f5873b6e2f7d89e42
Andrey Koltsov <andreykoltsov@Andreys-MacBook-Air.local>
2022-08-06 17:55:21 +0300

    Add basic script for visualization.

---
+++ visualization/README.md
@@ -0,0 +1,3 @@
+# installation
+ python3 -m pip install -U pip && python3 -m pip install -U matplotlib
+
---
+++ visualization/visualize.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+# Define data values
+x = [7, 14, 21, 28, 35, 42, 49]
+y = [5, 12, 19, 21, 31, 27, 35]
+z = [3, 5, 11, 20, 15, 29, 31]
+
+# Plot a simple line chart
+plt.plot(x, y)
+
+# Plot another line on the same chart/graph
+plt.plot(x, z)
+
+plt.show()
