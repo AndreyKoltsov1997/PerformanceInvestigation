3639d68f789d0711663f13dbbdbe51908aa37a01
Andrey Koltsov <andreykoltsov@Andreys-MacBook-Air.local>
2022-08-06 20:56:45 +0300

    Add support for multiple percentiles.

--- visualization/visualize.py
+++ visualization/visualize.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 Describes single measurement from JMH.
 """
 class BenchmarkMeasurement:
-    def __init__(self, sample_size, percentile, value, measurement):
+    def __init__(self, sample_size: str, percentile: str, value: str, measurement: str):
         self.sample_size = int(sample_size)
         self.percentile = percentile
         # -- JMH prints results as "0,124", "1,123" and all of them are integer. ...
@@ -19,12 +19,13 @@ class BenchmarkMeasurement:
         self.measurement = measurement
 
 
-def __get_payload_from_line(line: str) -> BenchmarkMeasurement:
+def __get_jmh_measurement_from_line(line: str) -> BenchmarkMeasurement:
     """
     Example of line: ...
     # CalculatorBenchmark.runEnhancedBenchmark:runEnhancedBenchmark�p0.95   1000  sample   1,217   ms/op
     :param group: - test name
     :param line: - raw output of JMH
+    :returns: BenchmarkMeasurement instance
     """
     payload = line.split()
     # 5 - minimal amount of elements within valuable payload
@@ -37,103 +38,68 @@ def __get_payload_from_line(line: str) -> BenchmarkMeasurement:
     return BenchmarkMeasurement(sample_size=payload[1], percentile=f"{percentile}th", value=payload[3], measurement=payload[4])
 
 
-def __get_samples(sample_size: str, percentile: str, object: list) -> list:
-    final_list = list(filter(lambda measure: measure.sample_size == sample_size and measure.percentile == percentile, object))
-    result = list(map(lambda x: x.value, final_list))
-    return result
+def __get_jmh_measurement_matching_criteria(measurements: list[BenchmarkMeasurement], sample_size: int, percentile: str) -> BenchmarkMeasurement:
+    """
+    Retrieves JMH measurement based on provided criterias.
+    :param measurements: - list of JMH measurements
+    :param sample_size: - JMH sample size used within measurement
+    :param percentile: - percentile of JMH result
+    """
+    samples = list(filter(lambda measure: (measure.percentile == percentile and measure.sample_size == sample_size), measurements))
+    if (len(samples) > 1):
+        # unexpected - JMH uses unique percentiles
+        raise ValueError(f"Found more than 1 sample for given percentile: {percentile}")
+    return samples[0]
 
-
-
-def __get_data_as_dict(file_path) -> dict:
-    # sample size, percentile, value, group name
-    # Sample Size <-> [Objects]
+def __get_jmh_measurements_from_file(file_path: str) -> dict:
+    """
+    Parses file that contains JMH measurements.
+    :param file_path: - path to file with measurements
+    :returns: dictionary: <sample size> - <[measurements]
+    """
     data = defaultdict(list)
 
     with open(file_path) as file:
         for line in file:
             try:
-                jmh_measurement = __get_payload_from_line(line)
+                jmh_measurement = __get_jmh_measurement_from_line(line)
             except Exception as e:
                 print(f"Unable to parse line: {line} \n {e}")
                 continue
             
-            if not jmh_measurement or jmh_measurement.percentile != '95th':
+            if not jmh_measurement:
                 continue
             data[jmh_measurement.sample_size].append(jmh_measurement)
     return data
 
 
-def __print_data(file_path) -> None:
-    # sample size, percentile, value, group name
-    # Sample Size <-> [Objects]
-    data = defaultdict(list)
-
-    with open(file_path) as file:
-        for line in file:
-            try:
-                jmh_measurement = __get_payload_from_line(line)
-            except Exception as e:
-                print(f"Unable to parse line: {line} \n {e}")
-                continue
-            
-            if not jmh_measurement or jmh_measurement.percentile != '95th':
-                continue
-            data[jmh_measurement.sample_size].append(jmh_measurement)
-
-    # Plot a simple line chart
-    # <sample-duration> for each percentile
-    # samples should be constant for all charts, it's only durations that differ
-
-    x_values = set()
-    y_values = list()
-    for sample_size, jmh_measurements in data.items():
-        x_values.add(sample_size)
-
-    # 1. Sort x values
-    # 2. Get y value for each x value
-    # 3. print
-    x_values = sorted(x_values)
-    y_values = []
-    for size in x_values:
-        y_values.append(data[size][0].value)
-    plt.plot(sorted(x_values), y_values, marker='o', label=file_path)
-
-
-def plot_files_via_dict(datasource_path: str) -> None:
+def plot_files_via_dict(datasource_path: str, percentile: int) -> None:
     """
     1. Parses files from given folders and retrieves JMH measurements.
     2. Plots measurements based on parsed data.
+    :param datasource_path: - path to folder where JMH measurements are locatedl
+    :param percentile: - target percentile
     """
     folder = os.fsencode(datasource_path)
 
     # -- Get data
-    plottable_data = defaultdict()
+    fname_with_plottable_data = defaultdict()
     for file in os.listdir(folder):
         filename_decoded = file.decode('utf-8')
         filepath = f"{datasource_path}/{filename_decoded}"
-        plottable_data[filename_decoded] = __get_data_as_dict(filepath)
+        fname_with_plottable_data[filename_decoded] = __get_jmh_measurements_from_file(filepath)
     
     # -- Visualize Data
-    for filename, jmh_measurements in plottable_data.items():
-        x_values = sorted(jmh_measurements.keys())
+    for filename, sample_size_to_jmh_data in fname_with_plottable_data.items():
+        x_values = sorted(sample_size_to_jmh_data.keys())
         y_values = list()
         for size in x_values:
-            y_values.append(jmh_measurements[size][0].value)
+            # -- get certain percentile for given sample size
+            measurement = __get_jmh_measurement_matching_criteria(sample_size_to_jmh_data[size], size, f"{percentile}th")
+            y_values.append(measurement.value)
         plt.plot(sorted(x_values), y_values, marker='o', label=filename)
 
-
-    plt.xlabel('Sample size')
-    plt.ylabel('Score, ms/op')
-    plt.legend(loc='upper center')
-    plt.show()
-
-def plot_files(datasource_path: str) -> None:
-    folder = os.fsencode(datasource_path)
-
-    for file in os.listdir(folder):
-        filepath = f"{datasource_path}/" + file.decode('utf-8')
-        __print_data(filepath)
-
+    plt.title(f"Java Microbenchmark Harness, {percentile}th percentile")
     plt.xlabel('Sample size')
     plt.ylabel('Score, ms/op')
     plt.legend(loc='upper center')
@@ -143,19 +109,32 @@ def plot_files(datasource_path: str) -> None:
 def main(argv):
     try:
         # -- parse CLI options
-        opts, args = getopt.getopt(argv, 'hd:', ['dir='])
+        opts, args = getopt.getopt(argv, 'hd:p:', ['directory=', 'percentile='])
         print(opts)
         if not opts:
             raise getopt.GetoptError('Not enough arguments.')
     except (getopt.GetoptError, ArgumentError) as e:
-      print(f"{e} \n Usage: python3 visualize.py -d <source directory>")
-      sys.exit(2)
+        print(f"{e} \n Usage: python3 visualize.py -d <source directory> -p <percentile>")
+        sys.exit(2)
     
     # -- actions based on CLI options
+    usage = 'python3 visualize.py -d <source directory> -p <percentile>'
+    source_directory = ''
+    target_percentile = ''
     for opt, arg in opts:
-        if opt == '-d':
-            plot_files_via_dict(arg)
-            sys.exit(0)
+        if opt == '-h':
+            print(usage)
+            sys.exit()
+        elif opt == '-p':
+            target_percentile = arg
+        elif opt == '-d':
+            source_directory = arg
+    
+    # -- print 95th percentile by default
+    target_percentile = int(target_percentile) if target_percentile else 95
+    if not source_directory:
+        raise ValueError(f"Source directory not specified. \n {usage}")
+    plot_files_via_dict(source_directory, int(target_percentile))
 
 
 if __name__ == "__main__":
